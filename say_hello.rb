feb27142f79c5af9edb9ff137c39d345e6eba28d
Eulis Bonifacio <eulis01@gmail.com>
2020-04-15 02:14:40 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
-def say_hello(name = "Kent Beck")
+def say_hello(name = "Ruby Programmer")
  puts "Hello #{name}!"
 end
-say_hello
+say_hello("Eulis")
 
 def say_hello(name = "Ruby Programmer!")
   puts "Hello #{name}"
